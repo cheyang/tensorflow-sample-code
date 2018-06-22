82df6ad7322a1dbff6a9ea4e51ee45b5bab6f3f8
cheyang <cheyang@163.com>
2018-06-22 19:42:13 +0800

    support sample

--- tfjob/docker/mnist-client/mnist_client.py
+++ tfjob/docker/mnist-client/mnist_client.py
@@ -38,13 +38,17 @@ stub = prediction_service_pb2.beta_create_PredictionService_stub(channel)
 
 request = predict_pb2.PredictRequest()
 request.model_spec.name = "mnist"
-request.model_spec.signature_name = "serving_default"
+request.model_spec.signature_name = "predict_images"
 request.inputs['images'].CopyFrom(
-    tf.contrib.util.make_tensor_proto(image, shape=[1, 28, 28]))
+    tf.contrib.util.make_tensor_proto(image, shape=[1, 784]))
 
 result = stub.Predict(request, 10.0)  # 10 secs timeout
 
-print(result)
+# print(result)
 print(MNIST.display(image, threshold=0))
+response = numpy.array(
+          result.outputs['scores'].float_val)
+prediction = numpy.argmax(response)
+# print(prediction)
 print("Your model says the above number is... %d!" %
-      result.outputs["classes"].int_val[0])
+     prediction)
